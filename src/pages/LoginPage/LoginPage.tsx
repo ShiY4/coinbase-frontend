e67b31788428bb17f4c702cc07e0bfb8e2f016b9
Shitkin Ilya <iliya.shitkin@neuro-city.ru>
2025-12-23 12:13:25 +0300

    SCSS configured

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,9 +2,9 @@ import styles from './loginPage.module.scss';
 
 export function LoginPage() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.wrappertest1}>asdasd</div>
-      <div className={styles.wrappertest2}>asdaasd</div>
+    <div className={styles.loginPageWrapper}>
+      asdasd
+      <div></div>
     </div>
   );
 }
